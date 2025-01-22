c9a2f9284bba4be4fbc17910ad3787c1d129ffea
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2025-01-22 22:33:42 +0530

    project setup:
    ACII art added
    added initial definition
    prompt configured

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+mod pentry;
+
+use crate::pentry::prompt;
+use crate::pentry::read_passwords_file;
+
+
+fn clr() {
+    print!("{}[2J", 27 as char);
+}
+
 fn main() {
+    clr();
     println!("Hello, world!");
+
+    let ascii = r#"
+____    ____  ___       __       __    __  .___________. _______ .______
+\   \  /   / /   \     |  |     |  |  |  | |           ||   ____||   _  \
+ \   \/   / /  ^  \    |  |     |  |  |  | `---|  |----`|  |__   |  |_)  |
+  \      / /  /_\  \   |  |     |  |  |  |     |  |     |   __|  |      /
+   \    / /  _____  \  |  `----.|  `--'  |     |  |     |  |____ |  |\  \----.
+    \__/ /__/     \__\ |_______| \______/      |__|     |_______|| _| `._____|
+
+    "#;
+
+    println!("{ascii}");
+
+    loop {
+        println!("Available Options:-");
+        println!("1. Add a new password");
+        println!("2. View all passwords");
+        println!("3. Search for a password");
+        println!("4. Exit");
+
+
+        let mut choice = String::new();
+
+        std::io::stdin().read_line(&mut choice).unwrap();
+
+        match choice.trim() {
+            "1" => {
+
+            }
+            "2" => {
+
+            }
+            "3" => {
+
+            }
+            "4" => {
+
+            }
+            _ => {
+
+            }
+        };
+    }
+
 }
---
+++ src/pentry.rs
@@ -0,0 +1,12 @@
+// mod pentry;
+//
+// use crate::pentry::prompt;
+// use crate::read_passwords_file;
+
+pub fn prompt() {
+
+}
+
+pub fn read_passwords_file() {
+
+}
